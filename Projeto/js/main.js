dabe023e0a39f75152188d106b01c1739a9176d8
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2020-10-02 21:39:09 -0300

    Add files via upload
    Entrega Final

--- Projeto/js/main.js
+++ Projeto/js/main.js
@@ -9,15 +9,19 @@ function trocaDiv(n) {
   mostraDiv(sIndex += n);
 }
 
-// Função que pega o numero do slide e faz a troca de um por outro toda vez que 
+// Função que pega o numero do slide (n) e faz a troca de um por outro toda vez que 
 // o botao de troca é acionado.
 function mostraDiv(n) {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  if (n > x.length) {sIndex = 1}
-  if (n < 1) {sIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[sIndex-1].style.display = "block";  
+  //definindo variavel i
+  var i
+  //variavel x é atribuida ao elemento com a classe "slides"
+  var x = document.getElementsByClassName("slides")
+  //se a variavel n for maior que o comprimento de x a variavel sIndex vai ser atribuida a 1
+  if (n > x.length) { sIndex = 1 }
+  //se a variavel n for menor que 1 a variavel sIndex vai ser tribuida ao comprimento da variavel x
+  if (n < 1) { sIndex = x.length }
+  //utilizando o laço for para aumentar o valor do comprimento de x e mostrar 
+  //a proxima imagem dentro da div.
+  for (i = 0; i < x.length; i++) { x[i].style.display = "none" }
+  x[sIndex - 1].style.display = "block";
 }
